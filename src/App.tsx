8d5c9dea215dad3f0cd75f3fea05db0c725cb78c
Ivan Huzhva <iwan_guzhwa@gmail.com>
2025-10-13 11:18:44 +0200

    clear version

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
+import { Outlet } from 'react-router-dom';
+import styles from './styles/index.module.scss';
+import { Footer } from './modules/Footer';
 import React from 'react';
-import './App.scss';
-
-interface Props {
-  onClick: () => void;
-  children: React.ReactNode;
-}
-
-export const Provider: React.FC<Props> = React.memo(({ onClick, children }) => (
-  <button type="button" onClick={onClick}>
-    {children}
-  </button>
-));
+import { useMyContext } from './Context/ProductContexts';
 
 export const App: React.FC = () => {
+  const { isMenuOpen } = useMyContext();
+
   return (
-    <div className="starter">
-      <Provider onClick={() => ({})}>TodoList</Provider>
+    <div className={styles.app}>
+      <title>best app</title>
+      <h1 className={styles.sr_only}>Product Catalog</h1>
+
+      <main className={styles.content}>
+        <Outlet />
+      </main>
+      {!isMenuOpen && <Footer />}
     </div>
   );
 };
